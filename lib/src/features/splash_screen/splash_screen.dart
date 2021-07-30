bced7782213ef824319dec108f561fb4f671f921
Yuliia Koba <yuliia.koba@nure.ua>
2021-07-30 17:10:00 +0300

    FL-14 Base architecture
    * Add BaseWidget
    * Add BaseState
    * Add BaseInteractors
    * Add MaterialApp
    * Add NamedRoutes (navigation)
    * Add Theme

---
+++ lib/src/features/splash_screen/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  static String get screenName => 'splash_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
